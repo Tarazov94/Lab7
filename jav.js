6ac41ff72d6f498582b00f0041a072d2ccf5500d
Tarazov94 <tarazov94@gmail.com>
2015-05-28 01:44:31 +0500

    Create jav.js
    Таразов Айнур Фанисович
    АСУ-12-1б
    Лабораторная работа №6(MVC) 
    Создал страницу с AJAXом.
    AJAX использован для создания 
    часов и элемента лайков.

---
+++ jav.js
@@ -0,0 +1,33 @@
+function show()  
+        {  
+            $.ajax({  
+                url: "time.php",  
+                cache: false,  
+                success: function(html){  
+                    $("#content2").html(html);  
+                }  
+            });  
+        }
+$(document).ready(function(){  
+            show();  
+            setInterval('show()',1000);  
+        }); 
+
+$(document).ready(function(){ 
+   // alert('#pv_like_wrap');
+   var b = document.getElementById('pv_like_icon');
+            $('#pv_like_wrap').click(function(){
+                b.style.zoom=1.5;
+                b.style.marginTop='3px';
+                var c = document.getElementById('zamet');
+                $.ajax({  
+                    url: "like.php?id="+c.value,  
+                    cache: false,  
+                    success: function(html){  
+                        $("#pv_like_count").html(html);
+                        b.style.zoom=1;
+                        b.style.marginTop='6px';
+                    }  
+                });  
+            });  
+        });
